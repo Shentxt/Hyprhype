1cf129e0e42b5422d48f3c0955a6c7ffce0a6763
Shentxt <fedezomber35@gmail.com>
2023-09-24 18:26:06 -0300

    Add existing file

---
+++ .config/ags/js/bar/TopBar.js
@@ -0,0 +1,93 @@
+import OverviewButton from './buttons/OverviewButton.js';
+import Workspaces from './buttons/Workspaces.js';
+import FocusedClient from './buttons/FocusedClient.js';
+import MediaIndicator from './buttons/MediaIndicator.js';
+import DateButton from './buttons/DateButton.js';
+import NotificationIndicator from './buttons/NotificationIndicator.js';
+import SysTray from './buttons/SysTray.js';
+import ColorPicker from './buttons/ColorPicker.js';
+import SystemIndicators from './buttons/SystemIndicators.js';
+import PowerMenu from './buttons/PowerMenu.js';
+import Separator from '../misc/Separator.js';
+import ScreenRecord from './buttons/ScreenRecord.js';
+import BatteryBar from './buttons/BatteryBar.js';
+import SubMenu from './buttons/SubMenu.js';
+const { Window, CenterBox, Box } = ags.Widget;
+const { SystemTray } = ags.Service;
+
+const submenuItems = ags.Variable(1);
+SystemTray.instance.connect('changed', () => {
+    submenuItems.setValue(SystemTray.items.length + 1);
+});
+
+const SeparatorDot = (service, condition) => Separator({
+    orientation: 'vertical',
+    valign: 'center',
+    connections: service && [[ags.Service[service], dot => {
+        dot.visible = condition(ags.Service[service]);
+    }]],
+});
+
+const Start = () => Box({
+    className: 'start',
+    vertical: true,
+    children: [
+        OverviewButton(),
+        SeparatorDot(),
+        Workspaces(),
+        SeparatorDot(),
+        FocusedClient(),
+        Box(),
+        NotificationIndicator(),
+        SeparatorDot('Notifications', n => n.notifications.length > 0 || n.dnd),
+    ],
+});
+
+const Center = () => Box({
+    className: 'center',
+    vertical: true,
+    children: [
+        DateButton(),
+    ],
+});
+
+const End = () => Box({
+    className: 'end',
+    vertical: true,
+    children: [
+        SeparatorDot('Mpris', m => m.players.length > 0),
+        MediaIndicator(),
+        Box({}),
+
+        SubMenu({
+            items: submenuItems,
+            vertical: true,
+            children: [
+                SysTray(),
+                ColorPicker(),
+            ],
+        }),
+        SeparatorDot(),
+        ScreenRecord(),
+        SeparatorDot('Recorder', r => r.recording),
+        SystemIndicators(),
+        SeparatorDot('Battery', b => b.available),
+        BatteryBar(),
+        SeparatorDot(),
+        PowerMenu(),
+    ],
+});
+
+export default monitor => Window({
+    name: `bar${monitor}`,
+    exclusive: true,
+    monitor,
+    anchor: 'top left bottom',
+    child: CenterBox({
+        vertical: true,
+        className: 'panel',
+        startWidget: Start(),
+        centerWidget: Center(),
+        endWidget: End(),
+    }),
+});
